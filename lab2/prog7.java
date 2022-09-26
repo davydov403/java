1062910365a88dfd3099ed3c7ce3f2dac75dbaee
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:26:09 +0700

    Create prog7.java

---
+++ lab2/prog7.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog7 {
+public static void main(String[] args) {
+//        String s = "Happy";
+//        System.out.println("New Year");
+//        System.out.println("Happy New");
+//        System.out.println("Year");
+//        System.out.print("Happy New");
+//        System.out.println("Year");
+//        System.out.println(s);
+//        System.out.print("Happy New!");
+//        System.out.print(" ");
+//        System.out.println("Year");
+//        String s = "Happy";
+//        System.out.println("New Year");
+//        System.out.println("Happy New");
+//        System.out.println("Year");
+    System.out.print("Happy New");
+//      System.out.println("Year");
+//        System.out.println(s);
+//        System.out.print("Happy New!");
+    System.out.print(" ");
+    System.out.println("Year");
+    }    
+}
