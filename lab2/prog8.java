3dc0160e883186cd4fed685d582e4f805ae88864
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:27:05 +0700

    Create prog8.java

---
+++ lab2/prog8.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog8 {
+public static void main(String[] args) {
+        //String name = "Elly";
+        //String text = "Hello " + name + "!";
+        //System.out.println(text);
+        String name = "Amigo";
+        String text = "Hello " + name + "!";
+        System.out.println(text);
+    }    
+}
