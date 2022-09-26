384edf5594fc6b8826d55f4a3713abbaf5363626
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:22:57 +0700

    Create prog5.java

---
+++ lab2/prog5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog5 {
+ public static int a = 1;
+ public static int b = 3;
+ public static int c = 9;
+ public static int d = 27;
+ public static void main(String[] args) {
+       // int result = -a - b - c - d;
+        int result = -a +b - c +d;
+        System.out.println(result);
+    }
+}
