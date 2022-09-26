46890698264e06d571b80469087758217c3c021a
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:24:48 +0700

    Create prog6.java

---
+++ lab2/prog6.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog6 {
+public static void main(String[] args) {
+    String a = "JavaRush";
+    String b = "use anywhere";
+    int i;
+    for (i=1;i<=10;i++) {
+    System.out.println(a+". learn once -"+b); }   
+    }    
+}
