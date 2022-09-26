2a725a87750445b6fcac0bfd3552f5a63f146ae4
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:11:52 +0700

    Create prog1.java

---
+++ lab2/prog1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog1 {
+  public static void main(String[] args){
+    int a = 2;
+    int b = 3;
+    System.out.println(a + b);
+    }    
+}
