c739493d90c27e3b43f8303e29ecd7df515d411e
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:29:16 +0700

    Create prog10.java

---
+++ lab2/prog10.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog10 {
+   public static void main(String[] args) {
+        //int a = 3;
+        //int a = 10;
+       // int b = 6;
+        //int b = 12;
+        //int summa = 1 + a + b;
+        //int summa = 2 + a + b;
+        //int summa = 3 + a + b;
+        //int summa = 4 + a + b;
+       // System.out.println("summa = " + summa);
+        int a = 3;
+       // int a = 10;
+        int b = 6;
+       // int b = 12;
+       // int summa = 1 + a + b;
+        //int summa = 2 + a + b;
+          int summa = 3 + a + b;
+        //int summa = 4 + a + b;
+        System.out.println("summa = " + summa);
+    }    
+}
