d9a3cee79357dd6b6a3641f5cc4f93c105efc570
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:14:59 +0700

    Create prog3.java

---
+++ lab2/prog3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog3 {
+ public static void main(String[] args){
+    int a = 5;
+    int b = 6;
+    String s = "Sum:";
+    System.out.print(""+s);
+    System.out.println(a+b);        
+    }    
+}
