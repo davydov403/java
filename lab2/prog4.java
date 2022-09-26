4f071f9d58633466110f0f3dcf96a1c0e1a34a84
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:19:44 +0700

    Create prog4.java

---
+++ lab2/prog4.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.Scanner;
+public class prog4 {
+ public static void main(String[] args){
+     int a;
+     Scanner s = new Scanner(System.in);
+     a = s.nextInt();
+     System.out.println(a*a);
+     }    
+}
