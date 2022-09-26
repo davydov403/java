8a8b725674ddaed564fe5490f39f2cef0536b26d
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-09-26 17:27:56 +0700

    Create prog9.java

---
+++ lab2/prog9.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.davydov_403.lab2;
+
+/**
+ *
+ * @author Admin
+ */
+public class prog9 {
+public static void main(String[] args) {
+        String s1 = "Coding ";
+        String s2 = "in ";
+        String s3 = "java";
+        s1 = s1 + s2 + s3;
+        System.out.println(s1);
+    }    
+}
