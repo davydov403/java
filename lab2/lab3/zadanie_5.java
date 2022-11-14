e5c663509778cbfd63868be8a1b741bcfc1f1a0f
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-11-14 20:44:30 +0700

    Create zadanie_5.java

---
+++ lab2/lab3/zadanie_5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Davydov
+ */
+public class zadanie_5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter a,c: ");
+        double a = in.nextInt();
+        double c = in.nextInt();
+        double b = Math.sqrt(c*c - a*a);
+        System.out.println(b);
+    } 
+}
