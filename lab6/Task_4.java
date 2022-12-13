0294886dcb6012e93a3cdf93717f2507627c131e
davydov403 <99866212+davydov403@users.noreply.github.com>
2022-12-13 19:20:49 +0700

    Create Task_4.java

---
+++ lab6/Task_4.java
@@ -0,0 +1,20 @@
+package lab6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Davydov
+ */
+public class Task_4 {
+    public static void main(String[] args) {
+        double dollars, rub;
+        final double course = 72.12;
+        Scanner input = new Scanner(System.in);
+        System.out.println("dollars: ");
+        dollars = input.nextDouble();
+        rub = dollars*course;
+        System.out.println("Rub from dollars: " + Math.ceil(rub));
+
+    }
+}
